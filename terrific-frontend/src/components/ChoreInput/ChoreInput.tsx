da0d561ed06f97b5459737a0af6637b2d3780882
Jesus Garcia <jesusgarhdez@hotmail.com>
2024-12-14 14:05:45 -0600

    HTML Template and components created

---
+++ terrific-frontend/src/components/ChoreInput/ChoreInput.tsx
@@ -0,0 +1,10 @@
+export default function ChoreInput() {
+    return(
+        <>
+            Add a new chore
+            <input type="text" />
+
+            <button>+</button>
+        </>
+    )
+}
